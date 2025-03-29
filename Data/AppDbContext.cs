0fff5312d1b2c4814f66646be233c43fc5cb989f
altair <ar80pro@gmail.com>
2025-03-29 15:19:18 +0200

    configure EF Core -  add Contact Model

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using contactNumbersManager.Models;
+using Microsoft.EntityFrameworkCore;
+namespace contactNumbersManager.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace contactNumbersManager.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^01\d{9}$", ErrorMessage = "Phone number must start with '01' and be exactly 11 digits.")]
+        public string Phone { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [StringLength(500, ErrorMessage = "Notes cannot be longer than 500 characters.")]
+        public string Notes { get; set; }
+    }
+}
